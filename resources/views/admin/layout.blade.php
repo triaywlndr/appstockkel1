56f45d0b21f193ea563d3f2a1213350ae356ab94
unknown <ayu32674@gmail.com>
2024-02-26 16:11:35 +0700

    view

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -63,7 +63,9 @@
     </div>
   </div>
     </div>
-    <div class="col-9">.col-6<br>Subsequent columns continue along the new line.</div>
+    <div class="col-9">
+       @yield('conten')
+    </div>
   </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
