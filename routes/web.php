4bbf977dbd41cf95786758018be6f23cf586eace
unknown <pctrizkywijaya@gmail.com>
2024-02-29 16:19:54 +0700

    rizky

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,22 @@ Route::get('/tokoh', function () {
     return view('admin.layout');
 });
 
+//admin
 Route::get('/admin', function () {
     return view('admin.admin');
 });
-
 Route::get('/tambahstock', [App\Http\Controllers\stock::class, 'create']);
 Route::post('/savestock', [App\Http\Controllers\stock::class, 'store']);
 Route::get('/edit{id}', [App\Http\Controllers\stock::class, 'edit']);
 Route::put('/updatestock{id}', [App\Http\Controllers\stock::class, 'update']);
 Route::get('/delete{id}', [App\Http\Controllers\stock::class, 'destroy']);
 
+//supplier 
 Route::get('/supplier', function () {
     return view('supplier.supplier');
 });
+Route::get('/tambahsupplier', [App\Http\Controllers\suppler::class, 'create']);
+Route::get('/savesupplier ', [App\Http\Controllers\supplier::class, 'store']);
+Route::get('/edit{id} ', [App\Http\Controllers\supplier::class, 'edit']);
+Route::get('/updatesupplier{id} ', [App\Http\Controllers\supplier::class, 'update']);
+Route::get('/delete{id} ', [App\Http\Controllers\supplier::class, 'destroy']);
