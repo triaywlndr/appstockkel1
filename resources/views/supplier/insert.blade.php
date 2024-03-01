82a12d10bc815b305d9c61537341f87c16fa23c7
unknown <pctrizkywijaya@gmail.com>
2024-03-01 15:38:07 +0700

    Rizky

--- resources/views/supplier/insert.blade.php
+++ resources/views/supplier/insert.blade.php
@@ -6,7 +6,7 @@
     @csrf
   <div class="mb-3">
     <label for="nama" class="form-label">Nama Supplier</label>
-    <input type="text" class="form-control" id="nama" placeholder="Masukan Nama supplier" value="{{old('nama')}}">
+    <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukan Nama supplier" value="{{old('nama')}}">
     <div style="color:red">
       @error('nama')
           {{$message}}
@@ -15,12 +15,11 @@
   </div>
   <div class="mb-3">
     <label for="nomor" class="form-label">No. Whatsapp</label>
-    <input type="text" class="form-control" id="nomor" placeholder="Masukan No. Whatsapp" value="{{old('nomor')}}">
+    <input type="text" class="form-control" id="nomor" name="nomor" placeholder="Masukan No. Whatsapp" value="{{old('nomor')}}">
   </div>
   <div class="mb-3">
     <label for="alamat" class="form-label">Alamat Perusahaan</label>
-    <textarea name="alamat" class="form-control" id="alamat" cols="30" rows="10" value="{{old('alamat')}}">
-    </textarea>
+    <textarea name="alamat" class="form-control" id="alamat" name="alamat" cols="30" rows="10" value="{{old('alamat')}}"></textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
