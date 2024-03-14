d4076183c539f4dcf00c24949fe5ff2fa0372379
unknown <tugasedwara35@gmail.com>
2024-03-14 14:00:00 +0700

    edwar

---
+++ resources/views/barangkeluar/bkeluar.blade.php
@@ -0,0 +1,45 @@
+@extends('admin.layout')
+@section('conten')
+    <div class="container">
+        <div class="card-body">
+            <h1 class="card-title">Data Barang Keluar</h1>
+        </div>
+        <hr>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Tanggal Faktur</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Supplier</th>
+                    <th scope="col">Harga Beli</th>
+                    <th scope="col">Jumlah Masuk</th>
+                    <th scope="col">Tanggal Dibuat</th>
+                    <th scope="col">Admin</th>
+                    <th scope="col">Cabang</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($bkeluars as $bm)
+                 <tr>
+                    <th scope="row">{{$loop->iteration}}</th>
+                    <td>{{$bm->tglf }}</td>
+                    <td>{{$bm->stock_stock }}</td>
+                    <td>{{$bm->supplier_id }}</td>
+                    <td>{{$bm->mharga }}</td>
+                    <td>{{$bm->mjumlah }}</td>
+                    <td>{{$bm->kota }}</td>
+                    <td>{{$bm->tgld}}</td>
+                    <td><a href="/editbkeluar/{{$bm->id}}" class="btn btn-success">Edit</a></td>
+                    <td><a href="/deletew/{{$bm->id}}" class="btn btn-danger">Delete</a></td>
+                </tr>   
+                </tr>   
+                @endforeach
+            </tbody>
+        </table>
+        <hr>
+        <a href="/tambahpelanggan" class="btn btn-primary">Tambah barang keluar</a>
+    </div>
+    </div>
+    </div>
+@endsection
---
+++ resources/views/barangkeluar/insert.blade.php
@@ -0,0 +1,96 @@
+@extends('admin.layout')
+@section('conten')
+
+<h1>Riwayat Barang Keluar</h1>
+<hr>
+<form action="/savepelanggan" method="post" enctype="multipart/form-data">
+    @csrf
+  <div>
+    <label for="nomor" class="form-label">No</label>
+    <input type="text" class="form-control" id="nomor" name="nomor" placeholder="Masukan No" value="{{old('no')}}">
+    <div style=color:red>
+      @error('nomor')
+          {{$message}}
+      @enderror
+    </div>
+  </div>
+
+  <div>
+    <label for="nomor" class="form-label">Tanggal Faktur</label>
+    <input type="text" class="form-control" id="nomor" name="nomor" placeholder="Masukan tanggal" value="{{old('tanggalfaktur')}}">
+    <div style=color:red>
+      @error('nomor')
+          {{$message}}
+      @enderror
+    </div>
+  </div>
+
+  <div>
+    <label for="nama" class="form-label">Nama</label>
+    <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukan nama" value="{{old('nama')}}">
+    <div style=color:red>
+      @error('nama')
+          {{$message}}
+      @enderror
+    </div>
+  </div>
+
+    <div>
+        <label for="supplier" class="form-label">Supplier</label>
+        <input type="text" class="form-control" id="supplier" name="supplier" placeholder="supplier" value="{{old('supplier')}}">
+        <div style=color:red>
+          @error('supplier')
+              {{$message}}
+          @enderror
+        </div>
+    </div>
+
+    <div>
+        <label for="nomor" class="form-label">Harga Beli</label>
+        <input type="text" class="form-control" id="nomor" name="nomor" placeholder="nomor" value="{{old('hargabeli')}}">
+        <div style=color:red>
+          @error('nomor')
+              {{$message}}
+          @enderror
+        </div>
+    </div>
+    <div>
+        <label for="jumlah" class="form-label">Jumlah Masuk</label>
+        <input type="text" class="form-control" id="jumlah" name="jumlah" placeholder="jumlah" value="{{old('jumlahmasuk')}}">
+        <div style=color:red>
+          @error('jumlah')
+              {{$message}}
+          @enderror
+        </div>
+    </div>
+    <div>
+        <label for="tanggal" class="form-label">Tanggal Dibuat</label>
+        <input type="text" class="form-control" id="tanggal" name="tanggal" placeholder="tanggal" value="{{old('tanggaldibuat')}}">
+        <div style=color:red>
+          @error('tanggal')
+              {{$message}}
+          @enderror
+        </div>
+    </div>
+    <div>
+        <label for="admin" class="form-label">Admin</label>
+        <input type="text" class="form-control" id="admin" name="admin" placeholder="admin" value="{{old('admin')}}">
+        <div style=color:red>
+          @error('admin')
+              {{$message}}
+          @enderror
+        </div>
+    </div>
+    <div>
+        <label for="cabang" class="form-label">Cabang</label>
+        <input type="text" class="form-control" id="cabang" name="cabang" placeholder="cabang" value="{{old('cabang')}}">
+        <div style=color:red>
+          @error('cabang')
+              {{$message}}
+          @enderror
+        </div>
+    </div>
+      <hr>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+@endsection
