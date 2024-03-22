1192a25db561327df44015a5093c0e4e639b1773
unknown <tugasedwara35pct@gmail.com>
2024-03-22 14:31:28 +0700

    edwar

--- resources/views/barangkeluar/insert.blade.php
+++ resources/views/barangkeluar/insert.blade.php
@@ -6,46 +6,45 @@
 <form action="/savebkeluar" method="post" enctype="multipart/form-data">
     @csrf
   <div>
-    <label for="nomor" class="form-label">Tanggal Faktur</label>
-    <input type="date" class="form-control" id="nomor" name="nomor" placeholder="Masukan tanggal" value="{{old('tanggalfaktur')}}">
+    <label for="Tanggal Faktur" class="form-label">Tanggal Faktur</label>
+    <input type="date" class="form-control" id="nomor" name="ktglf" placeholder="Masukan tanggal" value="{{old('tanggalfaktur')}}">
     <div style=color:red>
-      @error('nomor')
+      @error('Tanggal Faktur')
           {{$message}}
       @enderror
     </div>
   </div>
     <div>
-        <label for="supplier" class="form-label">Supplier</label>
-        <input type="text" class="form-control" id="supplier" name="supplier" placeholder="supplier" value="{{old('supplier')}}">
+        <label for="Barang keluar" class="form-label">Supplier</label>
+        <input type="text" class="form-control" id="stock_id" name="stock_id" placeholder="stock_id" value="{{old('stock_id')}}">
         <div style=color:red>
-          @error('supplier')
-              {{$message}}
-          @enderror
-        </div>
-    </div>
-    <div>
-        <label for="nomor" class="form-label">Harga Beli</label>
-        <input type="text" class="form-control" id="nomor" name="nomor" placeholder="nomor" value="{{old('hargabeli')}}">
-        <div style=color:red>
-          @error('nomor')
+          @error('stock_id')
               {{$message}}
           @enderror
         </div>
     </div>
     <div>
         <label for="jumlah" class="form-label">Jumlah Masuk</label>
-        <input type="text" class="form-control" id="jumlah" name="jumlah" placeholder="jumlah" value="{{old('jumlahmasuk')}}">
+        <input type="text" class="form-control" id="jumlah" name="kjumlah" placeholder="jumlah" value="{{old('jumlahkeluar')}}">
         <div style=color:red>
-          @error('jumlah')
+          @error('jumlahkeluar')
               {{$message}}
           @enderror
         </div>
     </div>
     <div>
-        <label for="tanggal" class="form-label">Tanggal Dibuat</label>
-        <input type="date" class="form-control" id="tanggal" name="tanggal" placeholder="tanggal" value="{{old('tanggaldibuat')}}">
+        <label for="Nama pelanggan" class="form-label">Nama pelanggan</label>
+        <input type="text" class="form-control" id="pelanggan_id" name="pelanggan_id" placeholder="tanggal" value="{{old('tanggaldibuat')}}">
         <div style=color:red>
-          @error('tanggal')
+          @error('Nama Pelanggan')
+              {{$message}}
+          @enderror
+        </div>
+        <div>
+        <label for="Metode pembayaran" class="form-label">Metode pembayaran</label>
+        <input type="text" class="form-control" id="Metode pembayaran" name="pembayaran" placeholder="Metode pembayaran" value="{{old('Metode pembayaran')}}">
+        <div style=color:red>
+          @error('Metode pembayaran')
               {{$message}}
           @enderror
         </div>
