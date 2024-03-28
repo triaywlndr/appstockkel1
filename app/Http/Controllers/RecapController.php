77d83865d6dac3f4ccc517746f8275440e9775c9
unknown <pctrizkywijaya@gmail.com>
2024-03-28 14:18:57 +0700

    Rizky

--- app/Http/Controllers/RecapController.php
+++ app/Http/Controllers/RecapController.php
@@ -15,10 +15,24 @@ class RecapController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $bmasuk = Bmasuk::all();
-        $bkeluar = Bkeluar::all();
+        $tgl_mulai = $request->tgl_mulai;
+        $tgl_selesai = $request->tgl_selesai;
+        if ($tgl_mulai or $tgl_selesai) {
+
+            $bmasuk = Bmasuk::where('tglf', [$tgl_mulai, $tgl_selesai])->get();
+            $bkeluar = Bkeluar::where('ktglf', [$tgl_mulai, $tgl_selesai])->get();
+            $bkeluar = Bkeluar::where('created_at', [$tgl_mulai, $tgl_selesai])->sum('subtotal');
+
+            return view('recap.recap', compact('bmasuk', 'bkeluar'));
+        } else {
+
+            $bmasuk = Bmasuk::all();
+            $bkeluar = Bkeluar::all();
+            $sum_total = Bkeluar::sum('subtotal');
+            return view('recap.recap', compact('bmasuk', 'bkeluar'));
+        }
         return view('recap.recap', compact('bmasuk', 'bkeluar'));
     }
 
